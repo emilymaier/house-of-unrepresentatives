9fd2f54c2d32630cd7c6cc4ae642325d33b73aeb
Emily Maier <emily@emilymaier.net>
2014-10-31 14:58:54 -0400

    Better parsing and JSON output.

--- parsepdfs.sh
+++ parsepdfs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 mkdir temp/
+mkdir json/
+year=2000
 for f in pdfs/*
 do
 	pdftotext $f temp/$(basename $f)
-	./parsetext.py temp/$(basename $f)
+	./parsetext.py temp/$(basename $f) $year
+	year=$((year + 2))
 done
 rm -rfv temp/
--- parsetext.py
+++ parsetext.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#XXX: Redistricting notes cause multiple districts to be parsed together
 
+import json
 import re
 import sys
 import time
@@ -13,11 +13,12 @@ states = re.split(state_regex, election_string)
 
 all_districts = []
 for state_raw in states[1:51]:
-	state = state_raw.split("\nRecapitulation of Votes Cast in")[0].split("Total ..........")[0].split("\n..........")[0].split("\nPresidential electors ..........")[0].split("\n\u000c..........")[0]
+	state = state_raw.split("\nRecapitulation of Votes Cast in")[0].split("Total ..........")[0].split("\n..........")[0].split("\nPresidential electors ..........")[0].split("\n\u000c..........")[0].split("\n\u000cRepublican")[0]
 	candidates = []
 	votes = []
 	district = []
 	districts = []
+	orphaned_district_numbers = 0
 
 	for line in state.split("\n"):
 		if line == "AT LARGE" or \
@@ -36,7 +37,7 @@ for state_raw in states[1:51]:
 		   line[-10:] == "—Continued":
 			continue
 		if "......." in line:
-			candidates.append(line.split(" .")[0])
+			candidates.append(line.replace("1 ", "").split(" .")[0])
 			continue
 		try:
 			votes.append(int(line.replace(",", "").replace("1 ", "")))
@@ -46,12 +47,20 @@ for state_raw in states[1:51]:
 		if line == "(1 )" or line == "(2 )":
 			votes.append(1)
 			continue
+		try:
+			int(line[:-1])
+			orphaned_district_numbers += 1
+		except ValueError:
+			pass
 
 	for candidate in candidates:
 		candidate = candidate.rsplit(", ", 1)
 		if len(candidate) is 1:
 			candidate.append(candidate[0])
 			candidate[0] = ""
+		if (candidate[1] == "Republican" or len(district) > 0 and district[-1][1] != "Republican" and candidate[1] == "Democrat") and orphaned_district_numbers > 0:
+			orphaned_district_numbers -= 1
+			candidate[0] = "0. " + candidate[0]
 		if ". " in candidate[0]:
 			try:
 				int(candidate[0].split(". ")[0])
@@ -68,10 +77,29 @@ for state_raw in states[1:51]:
 
 state_names = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-for state in all_districts:
+apportionment_1992 = [7, 1, 6, 4, 52, 6, 6, 1, 23, 11, 2, 2, 20, 10, 5, 4, 6, 7, 2, 8, 10, 16, 8, 5, 9, 1, 3, 2, 2, 13, 3, 31, 12, 1, 19, 6, 5, 21, 2, 6, 1, 9, 30, 3, 1, 11, 9, 3, 9, 1]
+
+apportionment_2002 = [7, 1, 8, 4, 53, 7, 5, 1, 25, 13, 2, 2, 19, 9, 5, 4, 6, 7, 2, 8, 10, 15, 8, 4, 9, 1, 3, 3, 2, 13, 3, 29, 13, 1, 18, 5, 5, 19, 2, 6, 1, 9, 32, 3, 1, 11, 9, 3, 8, 1]
+
+apportionment_2012 = [7, 1, 9, 4, 53, 7, 5, 1, 27, 14, 2, 2, 18, 9, 4, 4, 6, 6, 2, 8, 9, 14, 8, 4, 8, 1, 3, 4, 2, 12, 3, 27, 13, 1, 16, 5, 5, 18, 2, 7, 1, 9, 36, 4, 1, 11, 10, 3, 8, 1]
+
+for state_index in range(0, 50):
+	state = all_districts[state_index]
 	print(state_names.pop(0) + ":")
-	for district in state:
+	if int(sys.argv[2]) >= 1992 and int(sys.argv[2]) < 2002:
+		current_apportionment = apportionment_1992
+	elif int(sys.argv[2]) >= 2002 and int(sys.argv[2]) < 2012:
+		current_apportionment = apportionment_2002
+	elif int(sys.argv[2]) >= 2012 and int(sys.argv[2]) < 2022:
+		current_apportionment = apportionment_2012
+	for district_index in range(0, current_apportionment[state_index]):
+		district = state[district_index]
+		print(district_index + 1, ":")
 		for candidate in district:
 			print("%30s\t%25s\t%d" % (candidate[0], candidate[1], candidate[2]))
 		print("")
 	print("")
+
+json_output = open("json/" + sys.argv[2] + ".json", "w")
+json_output.write(json.dumps(all_districts))
+json_output.close()
